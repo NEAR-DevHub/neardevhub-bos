0ba42548169a53c6a18bdf1f174d3b6fc46cbf50
Bo Yao <by677@nyu.edu>
2023-03-30 17:43:34 +0800

    Refactor with community.Layout

---
+++ src/gigs-board/pages/community/Discussions.jsx
@@ -0,0 +1,147 @@
+/* INCLUDE: "common.jsx" */
+const nearDevGovGigsContractAccountId =
+  props.nearDevGovGigsContractAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
+  (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
+
+function widget(widgetName, widgetProps, key) {
+  widgetProps = {
+    ...widgetProps,
+    nearDevGovGigsContractAccountId: props.nearDevGovGigsContractAccountId,
+    nearDevGovGigsWidgetsAccountId: props.nearDevGovGigsWidgetsAccountId,
+    referral: props.referral,
+  };
+  return (
+    <Widget
+      src={`${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.${widgetName}`}
+      props={widgetProps}
+      key={key}
+    />
+  );
+}
+
+function href(widgetName, linkProps) {
+  linkProps = { ...linkProps };
+  if (props.nearDevGovGigsContractAccountId) {
+    linkProps.nearDevGovGigsContractAccountId =
+      props.nearDevGovGigsContractAccountId;
+  }
+  if (props.nearDevGovGigsWidgetsAccountId) {
+    linkProps.nearDevGovGigsWidgetsAccountId =
+      props.nearDevGovGigsWidgetsAccountId;
+  }
+  if (props.referral) {
+    linkProps.referral = props.referral;
+  }
+  const linkPropsQuery = Object.entries(linkProps)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+  return `#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
+    linkPropsQuery ? "?" : ""
+  }${linkPropsQuery}`;
+}
+/* END_INCLUDE: "common.jsx" */
+
+/* INCLUDE: "communities.jsx" */
+const communities = {
+  "zero-knowledge": {
+    overviewId: 397,
+    eventsId: 401,
+    icon: "https://ipfs.near.social/ipfs/bafkreiajwq6ep3n7veddozji2djv5vviyisabhycbweslvpwhsoyuzcwi4",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreihgxg5kwts2juldaeasveyuddkm6tcabmrat2aaq5u6uyljtyt7lu",
+    title: "Zero Knowledge",
+    desc: "Building a zero knowledge ecosystem on NEAR.",
+  },
+  protocol: {
+    overviewId: 412,
+    eventsId: 413,
+    icon: "https://ipfs.near.social/ipfs/bafkreidpitdafcnhkp4uyomacypdgqvxr35jtfnbxa5s6crby7qjk2nv5a",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreicg4svzfz5nvllomsahndgm7u62za4sib4mmbygxzhpcl4htqwr4a",
+    title: "Protocol",
+    desc: "Supporting the ongoing innovation of the NEAR Protocol.",
+  },
+  tooling: {
+    overviewId: 416,
+    eventsId: 417,
+    icon: "https://ipfs.near.social/ipfs/bafkreie2eaj5czmpfe6pe53kojzcspgozebdsonffwvbxtpuipnwahybvi",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreiehzr7z2fhoqqmkt3z667wubccbch6sqtsnvd6msodyzpnf72cszy",
+    title: "Tooling",
+    desc: "Supporting the ongoing innovation of tooling.",
+  },
+  "contract-standards": {
+    overviewId: 414,
+    eventsId: 415,
+    icon: "https://ipfs.near.social/ipfs/bafkreiepgdnu7soc6xgbyd4adicbf3eyxiiwqawn6tguaix6aklfpir634",
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey",
+    title: "Contract Standards",
+    desc: "Coordinating the contribution to the NEAR dapp standards.",
+  },
+};
+/* END_INCLUDE: "communities.jsx" */
+
+if (!props.label) {
+  return <div>Loading ...</div>;
+}
+
+const postIdsWithLabels = (labels) => {
+  const ids = labels
+    .map(
+      (label) =>
+        Near.view(nearDevGovGigsContractAccountId, "get_posts_by_label", {
+          label,
+        }) ?? []
+    )
+    .map((ids) => new Set(ids))
+    .reduce((previous, current) => {
+      let res = new Set();
+      for (let id of current) {
+        if (previous.has(id)) {
+          res.add(id);
+        }
+      }
+      return res;
+    });
+  ids.delete(communities[props.label].overviewId);
+  ids.delete(communities[props.label].eventsId);
+  return [...ids].reverse();
+};
+const discussionsRequiredLabels = [props.label];
+const discussionRequiredPosts = postIdsWithLabels(discussionsRequiredLabels);
+
+const Discussions = (
+  <div>
+    <div class="row mb-2 justify-content-center">
+      <div class="col w-auto">
+        <small class="text-muted">
+          Required labels:
+          {discussionsRequiredLabels.map((label) => (
+            <a href={href("Feed", { label })} key={label}>
+              <span class="badge text-bg-primary me-1">{label}</span>
+            </a>
+          ))}
+        </small>
+      </div>
+    </div>
+    {widget("components.layout.Controls", {
+      labels: discussionsRequiredLabels,
+    })}
+    <div class="row">
+      <div class="col">
+        {discussionRequiredPosts.map((postId) =>
+          widget("components.posts.Post", { id: postId }, postId)
+        )}
+      </div>
+    </div>
+  </div>
+);
+
+return widget("components.community.Layout", {
+  label: props.label,
+  children: Discussions,
+});
