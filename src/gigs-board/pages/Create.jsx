ae493303ccb87102e26e13d2f63d52f5e495bf6c
Ben Reilly <benreilly0809@gmail.com>
2023-05-08 18:22:41 -0700

    ready freddy

--- src/gigs-board/pages/Create.jsx
+++ src/gigs-board/pages/Create.jsx
@@ -4,8 +4,8 @@ const nearDevGovGigsContractAccountId =
   props.nearDevGovGigsContractAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
-const nearDevGovGigsWidgetsAccountId = context.accountId;
-props.nearDevGovGigsWidgetsAccountId ||
+const nearDevGovGigsWidgetsAccountId =
+  props.nearDevGovGigsWidgetsAccountId ||
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
 function widget(widgetName, widgetProps, key) {
