92949ed0591b59ae79d2ebd7649a58784d7eb213
Ben Reilly <benreilly0809@gmail.com>
2023-05-16 12:11:24 -0700

    Feed and stuff

--- src/gigs-board/components/layout/Page.jsx
+++ src/gigs-board/components/layout/Page.jsx
@@ -49,7 +49,7 @@ return (
     {widget("components.layout.Banner")}
 
     {props.header}
-    {widget("components.layout.Controls")}
+    {/* {widget("components.layout.Controls")} */}
     {props.children}
   </>
 );
