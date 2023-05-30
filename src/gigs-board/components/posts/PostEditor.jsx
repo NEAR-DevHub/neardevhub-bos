c4b507b3e945d00ca541205350ab35a437f1e0d5
Vlad Frolov <frolvlad@gmail.com>
2023-05-30 23:39:37 +0200

    fix: Fixed reply functionality

--- src/gigs-board/components/posts/PostEditor.jsx
+++ src/gigs-board/components/posts/PostEditor.jsx
@@ -96,7 +96,7 @@ let grantNotify = Near.view("social.near", "is_write_permission_granted", {
 if (grantNotify === null) {
   return;
 }
-const onClick = () => {
+const onSubmit = () => {
   let labels = state.labelStrings;
   var body = {
     Comment: { description: state.description, comment_version: "V2" },
