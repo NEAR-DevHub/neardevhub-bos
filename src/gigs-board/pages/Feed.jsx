c0d1b0a0be4f9f22dd79c747d514b6eb0bde181b
Carina Akaia 🦾🦊🪐🚀 root.akaia( .near | .eth ) <carina@dmail.ai>
2023-06-07 23:23:32 +0400

    wip: Merge feed header into feed page

--- src/gigs-board/pages/Feed.jsx
+++ src/gigs-board/pages/Feed.jsx
@@ -51,41 +51,209 @@ function href(widgetName, linkProps) {
   }${linkPropsQuery}`;
 }
 /* END_INCLUDE: "common.jsx" */
+/* INCLUDE: "shared/mocks" */
+const communities = {
+  "zero-knowledge": {
+    overviewId: 397,
+    eventsId: 401,
 
-State.init({
-  propsLabel: props.label,
-  label: props.label,
-  author: props.author,
-});
+    icon: "https://ipfs.near.social/ipfs/bafkreiajwq6ep3n7veddozji2djv5vviyisabhycbweslvpwhsoyuzcwi4",
 
-// When rerendered with different props, State will be preserved, so we need to update the state when we detect that the props have changed.
-if (props.label !== state.propsLabel) {
-  State.update({
-    propsLabel: props.label,
-    label: props.label,
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreihgxg5kwts2juldaeasveyuddkm6tcabmrat2aaq5u6uyljtyt7lu",
+
+    title: "Zero Knowledge",
+    desc: "Building a zero knowledge ecosystem on NEAR.",
+    telegram: "NearZeroKnowledge",
+  },
+
+  protocol: {
+    overviewId: 412,
+    eventsId: 413,
+
+    icon: "https://ipfs.near.social/ipfs/bafkreidpitdafcnhkp4uyomacypdgqvxr35jtfnbxa5s6crby7qjk2nv5a",
+
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreicg4svzfz5nvllomsahndgm7u62za4sib4mmbygxzhpcl4htqwr4a",
+
+    title: "Protocol",
+    desc: "Supporting the ongoing innovation of the NEAR Protocol.",
+
+    integrations: {
+      github: {
+        kanban: {
+          boards: {
+            "18855b9c9f2-216091d-6484800b-42593f54-6102b48a": {
+              id: "18855b9c9f2-216091d-6484800b-42593f54-6102b48a",
+
+              columns: {
+                "18855f4a93e-76a9b704-14c3ebdb-1e6c0f05-22653630": {
+                  id: "18855f4a93e-76a9b704-14c3ebdb-1e6c0f05-22653630",
+                  description: "Lorem ipsum",
+                  labelSearchTerms: ["S-draft"],
+                  title: "Draft",
+                },
+              },
+
+              dataTypesIncluded: { Issue: false, PullRequest: true },
+              description: "Latest NEAR Enhancement Proposals by status",
+              repoURL: "https://github.com/near/NEPs",
+              title: "NEAR Protocol NEPs",
+            },
+          },
+        },
+      },
+    },
+
+    telegram: "NEAR_Protocol_Community_Group",
+  },
+
+  tooling: {
+    overviewId: 416,
+    eventsId: 417,
+
+    icon: "https://ipfs.near.social/ipfs/bafkreie2eaj5czmpfe6pe53kojzcspgozebdsonffwvbxtpuipnwahybvi",
+
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreiehzr7z2fhoqqmkt3z667wubccbch6sqtsnvd6msodyzpnf72cszy",
+
+    title: "Tooling",
+    desc: "Supporting the ongoing innovation of tooling.",
+    telegram: "NEAR_Tools_Community_Group",
+  },
+
+  "contract-standards": {
+    overviewId: 414,
+    eventsId: 415,
+
+    icon: "https://ipfs.near.social/ipfs/bafkreiepgdnu7soc6xgbyd4adicbf3eyxiiwqawn6tguaix6aklfpir634",
+
+    cover:
+      "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey",
+
+    title: "Contract Standards",
+    desc: "Coordinating the contribution to the NEAR dapp standards.",
+    telegram: "nearnft",
+  },
+};
+/* END_INCLUDE: "shared/mocks" */
+
+const Gradient = styled.div`
+   {
+    height: 250px;
+    text-align: center;
+    background: radial-gradient(
+      circle,
+      rgba(29, 55, 57, 1) 30%,
+      rgba(24, 24, 24, 1) 80%
+    );
+
+    font-family: Arial, sans-serif;
+  }
+
+  .text-primary-gradient {
+    color: #53fdca;
+    -webkit-text-fill-color: transparent;
+    background-image: linear-gradient(#8e76ba, #1ed2f0);
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+
+  .subtitle-above {
+    font-size: 18px;
+    letter-spacing: 1px;
+    font-family: Courier, monospace;
+  }
+
+  .subtitle-below {
+    font-size: 16px;
+  }
+
+  .slogan {
+    font-weight: 600;
+    font-size: 60px;
+  }
+`;
+
+const header = (
+  <div className="d-flex flex-column">
+    <Gradient className="d-flex flex-column justify-content-center">
+      <div className="subtitle-above text-white opacity-75 mb-2">
+        A decentralized community of
+      </div>
+
+      <h1 className="mb-3 text-white slogan">
+        <span className="text-primary-gradient">NEAR </span>Developers
+      </h1>
+
+      <div className="subtitle-below text-white opacity-75">
+        Share your ideas, match solutions, and access support and funding.
+      </div>
+    </Gradient>
+
+    <div className="d-flex flex-column gap-4 py-4">
+      <div className="d-flex justify-content-between">
+        <h5 className="h5 m-0">Featured Communities</h5>
+      </div>
+
+      <div className="row">
+        {Object.entries(communities).map(([label, community]) => (
+          <div className="col">
+            {widget("entity.community.card", { label, ...community }, label)}
+          </div>
+        ))}
+      </div>
+
+      <div className="d-flex justify-content-end">
+        <a
+          className="btn btn-outline-secondary d-flex gap-2"
+          href={href("communities")}
+        >
+          <i className="bi bi-arrow-right-circle-fill" />
+          <span>Discover more</span>
+        </a>
+      </div>
+    </div>
+
+    <div className="h5 pb-4">Activity</div>
+  </div>
+);
+
+const FeedPage = ({ author, label, recency }) => {
+  State.init({ propsLabel: label, label, author });
+
+  // When rerendered with different props, State will be preserved, so we need to update the state when we detect that the props have changed.
+  if (label !== state.propsLabel) {
+    State.update({
+      propsLabel: label,
+      label,
+    });
+  }
+
+  const onSearchLabel = (value) => {
+    State.update({ label: value });
+  };
+
+  const onSearchAuthor = (value) => {
+    State.update({ author: value });
+  };
+
+  return widget("components.layout.Page", {
+    header,
+
+    children: widget("components.posts.Search", {
+      children: widget("components.layout.Controls"),
+      recency,
+      label,
+      author,
+      //
+      labelQuery: { label },
+      onSearchLabel,
+      //
+      authorQuery: { author },
+      onSearchAuthor,
+    }),
   });
-}
-
-const onSearchLabel = (label) => {
-  State.update({ label });
 };
 
-const onSearchAuthor = (author) => {
-  State.update({ author });
-};
-
-return widget("components.layout.Page", {
-  header: widget("entity.community.FeedHeader"),
-  children: widget("components.posts.Search", {
-    children: widget("components.layout.Controls"),
-    recency: props.recency,
-    label: state.label,
-    author: state.author,
-    //
-    labelQuery: { label: state.label },
-    onSearchLabel,
-    //
-    authorQuery: { author: state.author },
-    onSearchAuthor,
-  }),
-});
+return FeedPage(props);
