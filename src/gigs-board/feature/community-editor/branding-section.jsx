948d6ec045492c3dc30f6fde08f0faafa5cd07fa
Carina Akaia 🦾🦊🪐🚀 root.akaia( .near | .eth ) <carina@dmail.ai>
2023-06-11 00:04:17 +0400

    fix: Adjust borders

--- src/gigs-board/feature/community-editor/branding-section.jsx
+++ src/gigs-board/feature/community-editor/branding-section.jsx
@@ -51,98 +51,12 @@ function href(widgetName, linkProps) {
   }${linkPropsQuery}`;
 }
 /* END_INCLUDE: "common.jsx" */
-/* INCLUDE: "shared/lib/form" */
-/**
- *! TODO: Extract into separate library module
- *! once `useForm` is converted into a form factory widget
- */
-const traversalUpdate = ({
-  input,
-  target: treeOrBranch,
-  path: [currentBranchKey, ...remainingBranch],
-  params,
-  via: nodeUpdate,
-}) => ({
-  ...treeOrBranch,
 
-  [currentBranchKey]:
-    remainingBranch.length > 0
-      ? traversalUpdate({
-          input,
-
-          target:
-            typeof treeOrBranch[currentBranchKey] === "object"
-              ? treeOrBranch[currentBranchKey]
-              : {
-                  ...((treeOrBranch[currentBranchKey] ?? null) !== null
-                    ? { __archivedLeaf__: treeOrBranch[currentBranchKey] }
-                    : {}),
-                },
-
-          path: remainingBranch,
-          via: nodeUpdate,
-        })
-      : nodeUpdate({
-          input,
-          lastKnownState: treeOrBranch[currentBranchKey],
-          params,
-        }),
-});
-
-const fieldDefaultUpdate = ({
-  input,
-  lastKnownState,
-  params: { arrayDelimiter },
-}) => {
-  switch (typeof input) {
-    case "boolean":
-      return input;
-
-    case "object":
-      return Array.isArray(input) && typeof lastKnownState === "string"
-        ? input.join(arrayDelimiter ?? ",")
-        : input;
-
-    case "string":
-      return Array.isArray(lastKnownState)
-        ? input.split(arrayDelimiter ?? ",").map((string) => string.trim())
-        : input;
-
-    default: {
-      if ((input ?? null) === null) {
-        switch (typeof lastKnownState) {
-          case "boolean":
-            return !lastKnownState;
-
-          default:
-            return lastKnownState;
-        }
-      } else return input;
-    }
-  }
-};
-
-const useForm = ({ stateKey: formStateKey }) => ({
-  formState: state[formStateKey],
-
-  formUpdate:
-    ({ path: fieldPath, via: fieldCustomUpdate, ...params }) =>
-    (fieldInput) =>
-      State.update((lastKnownState) =>
-        traversalUpdate({
-          input: fieldInput?.target?.value ?? fieldInput,
-          target: lastKnownState,
-          path: [formStateKey, ...fieldPath],
-          params,
-
-          via:
-            typeof fieldCustomUpdate === "function"
-              ? fieldCustomUpdate
-              : fieldDefaultUpdate,
-        })
-      ),
-});
-/* END_INCLUDE: "shared/lib/form" */
+const CardCover = styled.div`
+  border-top-left-radius: var(--bs-border-radius-xl) !important;
+  border-top-right-radius: var(--bs-border-radius-xl) !important;
+  height: calc(100% - 84px);
+`;
 
 const coverURL =
   "https://ipfs.near.social/ipfs/bafkreiaowjqxds24fwcliyriintjd4ucciprii2rdxjmxgi7f5dmzuscey";
@@ -150,28 +64,21 @@ const coverURL =
 const CommunityEditorBrandingSection = ({}) => {
   return (
     <div
-      class="card shadow rounded-4 w-100"
+      className="card shadow rounded-4 w-100"
       style={{ maxWidth: 896, height: 240 }}
     >
-      <div
+      <CardCover
         alt="Community card cover preview"
-        class="card-img-top rounded-top-4"
+        className="card-img-top"
         style={{
-          background: [
-            "center",
-            "/",
-            "cover",
-            "no-repeat",
-            `url(${coverURL})`,
-          ].join(" "),
-          height: "calc(100% - 84px)",
+          background: `center / cover no-repeat url(${coverURL})`,
         }}
       />
 
-      <div class="card-body">
-        <h5 class="h5">Community name</h5>
+      <div className="card-body p-4">
+        <h5 className="h5">Community name</h5>
 
-        <p class="card-text">Short bio, I guess</p>
+        <p className="card-text">Short bio, I guess</p>
       </div>
     </div>
   );
