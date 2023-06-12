d78e626a25072ea27325fb3615e4aa2d1d9c5850
Peter Salomonsen <petersalomonsen@runbox.com>
2023-06-12 17:46:42 +0000

    mention text

--- src/DevGov/Notification/Item/Left.jsx
+++ src/DevGov/Notification/Item/Left.jsx
@@ -46,9 +46,8 @@ function href(widgetName, linkProps) {
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
 
-  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${
-    linkPropsQuery ? "?" : ""
-  }${linkPropsQuery}`;
+  return `/#/${nearDevGovGigsWidgetsAccountId}/widget/gigs-board.pages.${widgetName}${linkPropsQuery ? "?" : ""
+    }${linkPropsQuery}`;
 }
 /* END_INCLUDE: "common.jsx" */
 
@@ -60,15 +59,14 @@ const type = props.type.split("/")[1];
 return props.type ? (
   <>
     {type == "like"
-      ? "liked"
+      ? "liked your"
       : type == "reply"
-      ? "replied"
-      : type == "edit"
-      ? "edited"
-      : type == "mention"
-      ? "mentioned"
-      : "???"}{" "}
-    your
+        ? "replied to your"
+        : type == "edit"
+          ? "edited your"
+          : type == "mention"
+            ? "mentioned you in their"
+            : "???"}{" "}
     <a className="fw-bold text-muted" href={href("Post", { id: props.post })}>
       Developer Governance post
     </a>
