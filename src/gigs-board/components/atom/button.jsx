029edbd33e9861c7fb5a25134e0cda421b7e78db
Carina Akaia 🦾🦊🪐🚀 root.akaia( .near | .eth ) <carina@dmail.ai>
2023-06-14 18:36:08 +0400

    wip: Redefine primary button styles

--- src/gigs-board/components/atom/button.jsx
+++ src/gigs-board/components/atom/button.jsx
@@ -1,6 +1,12 @@
 const ButtonRoot = styled.button`
   min-height: 42;
   line-height: 1.5;
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
+  transition: box-shadow 0.6s;
+
+  &:hover {
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
+  }
 
   &.btn-sm {
     min-height: 32;
@@ -10,6 +16,23 @@ const ButtonRoot = styled.button`
   &.btn-lg {
     min-height: 48;
     line-height: 1.7;
+  }
+
+  &.btn-primary {
+    --bs-btn-color: #fff;
+    --bs-btn-bg: #087990;
+    --bs-btn-border-color: #087990;
+    --bs-btn-hover-color: #fff;
+    --bs-btn-hover-bg: #087990;
+    --bs-btn-hover-border-color: #087990;
+    --bs-btn-focus-shadow-rgb: 61, 138, 253;
+    --bs-btn-active-color: #fff;
+    --bs-btn-active-bg: #087990;
+    --bs-btn-active-border-color: #087990;
+    --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+    --bs-btn-disabled-color: #fff;
+    --bs-btn-disabled-bg: #087990;
+    --bs-btn-disabled-border-color: #087990;
   }
 
   & *:first-child {
@@ -24,7 +47,7 @@ const ButtonRoot = styled.button`
 const Button = ({ classNames, label, ...restProps }) => (
   <ButtonRoot
     className={[
-      "btn shadow d-inline-flex align-items-center gap-2 rounded-pill p-2",
+      "btn d-inline-flex align-items-center gap-2 rounded-pill p-2",
       classNames.root ?? "btn-primary",
     ].join(" ")}
     style={{ width: "fit-content" }}
