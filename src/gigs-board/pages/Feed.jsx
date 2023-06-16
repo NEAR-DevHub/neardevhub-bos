c34efd39db8a62c6fb5734bd14adca73bbf2db80
Carina Akaia 🦾🦊🪐🚀 root.akaia( .near | .eth ) <carina@dmail.ai>
2023-06-16 18:14:00 +0400

    Render communities starting from the left

--- src/gigs-board/pages/Feed.jsx
+++ src/gigs-board/pages/Feed.jsx
@@ -141,7 +141,7 @@ const header = (
         <h5 className="h5 m-0">Featured Communities</h5>
       </div>
 
-      <div className="d-flex gap-4 justify-content-center">
+      <div className="d-flex gap-4">
         {(DevHub.get_all_communities() ?? [])
           .slice(0, 4)
           .map((community) =>
