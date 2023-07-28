f632f65bfc924c63778cff21c02ca341145490ea
Thomas <thomas@guntenaar.org>
2023-07-28 15:22:43 +0200

    fix: reverts changes made to sponsorship reply which created bugs

--- src/gigs-board/entity/post/PostEditor.jsx
+++ src/gigs-board/entity/post/PostEditor.jsx
@@ -79,8 +79,6 @@ const parentId = props.parentId ?? null;
 const postId = props.postId ?? null;
 const mode = props.mode ?? "Create";
 
-const isSponsorship = postType === "Sponsorship";
-
 const referralLabels = props.referral ? [`referral:${props.referral}`] : [];
 const labelStrings = (props.labels ?? []).concat(referralLabels);
 const labels = labelStrings.map((s) => {
@@ -99,8 +97,8 @@ initState({
   name: props.name ?? "",
   description: props.description ?? "",
   amount: props.amount ?? "",
-  token: props.token ?? "Near",
-  supervisor: props.supervisor ?? "neardevgov.near",
+  token: props.token ?? "",
+  supervisor: props.supervisor ?? "",
   githubLink: props.githubLink ?? "",
   warning: "",
   draftStateApplied: false,
@@ -115,7 +113,13 @@ let fields = {
   Idea: ["name", "description"],
   Submission: ["name", "description", "fund_raising"],
   Attestation: ["name", "description"],
-  Sponsorship: ["name", "description", "fund_raising"],
+  Sponsorship: [
+    "name",
+    "description",
+    "amount",
+    "sponsorship_token",
+    "supervisor",
+  ],
   Github: ["githubLink", "name", "description"],
 }[postType];
 
@@ -344,37 +348,72 @@ const nameDiv = (
   </div>
 );
 
-const descriptionDiv = (
-  <div className="col-lg-12  mb-2">
-    Description:
-    <br />
-    <textarea
-      value={state.description}
+const amountDiv = (
+  <div className="col-lg-6  mb-2">
+    Amount:
+    <input
       type="text"
-      rows={6}
-      className="form-control"
-      onInput={(event) => textareaInputHandler(event.target.value)}
-      onKeyUp={(event) => {
-        if (event.key === "Escape") {
-          State.update({ showAccountAutocomplete: false });
-        }
-      }}
-      onChange={(event) => State.update({ description: event.target.value })}
+      value={state.amount}
+      onChange={(event) => State.update({ amount: event.target.value })}
     />
-    {autocompleteEnabled && state.showAccountAutocomplete && (
-      <AutoComplete>
-        <Widget
-          src="near/widget/AccountAutocomplete"
-          props={{
-            term: state.text.split("@").pop(),
-            onSelect: autoCompleteAccountId,
-            onClose: () => State.update({ showAccountAutocomplete: false }),
-          }}
-        />
-      </AutoComplete>
-    )}
   </div>
 );
+
+const tokenDiv = (
+  <div className="col-lg-6  mb-2">
+    Tokens:
+    <input
+      type="text"
+      value={state.token}
+      onChange={(event) => State.update({ token: event.target.value })}
+    />
+  </div>
+);
+
+const supervisorDiv = (
+  <div className="col-lg-6 mb-2">
+    Supervisor:
+    <input
+      type="text"
+      value={state.supervisor}
+      onChange={(event) => State.update({ supervisor: event.target.value })}
+    />
+  </div>
+);
+
+const callDescriptionDiv = () => {
+  return (
+    <div className="col-lg-12  mb-2">
+      Description:
+      <br />
+      <textarea
+        value={state.description}
+        type="text"
+        rows={6}
+        className="form-control"
+        onInput={(event) => textareaInputHandler(event.target.value)}
+        onKeyUp={(event) => {
+          if (event.key === "Escape") {
+            State.update({ showAccountAutocomplete: false });
+          }
+        }}
+        onChange={(event) => State.update({ description: event.target.value })}
+      />
+      {autocompleteEnabled && state.showAccountAutocomplete && (
+        <AutoComplete>
+          <Widget
+            src="near/widget/AccountAutocomplete"
+            props={{
+              term: state.text.split("@").pop(),
+              onSelect: autoCompleteAccountId,
+              onClose: () => State.update({ showAccountAutocomplete: false }),
+            }}
+          />
+        </AutoComplete>
+      )}
+    </div>
+  );
+};
 
 const disclaimer = (
   <p>
@@ -390,9 +429,7 @@ const isFundraisingDiv = (
   <>
     <div class="mb-2">
       <p class="fs-6 fw-bold mb-1">
-        {isSponsorship
-          ? "Are you funding their solution?"
-          : "Are you seeking funding for your solution?"}
+        Are you seeking funding for your solution?
         <span class="text-muted fw-normal">(Optional)</span>
       </p>
       <div class="form-check form-check-inline">
@@ -450,24 +487,24 @@ const fundraisingDiv = (
       </select>
     </div>
     <div className="col-lg-6 mb-2">
-      {isSponsorship ? "Sponsored" : "Requested"} amount
+      Requested amount
       <span class="text-muted fw-normal">(Numbers Only)</span>
       <input
         type="number"
         value={parseInt(state.amount) > 0 ? state.amount : ""}
         min={0}
-        onChange={(event) =>
+        onChange={(event) => {
           State.update({
             amount: Number(
               event.target.value.toString().replace(/e/g, "")
             ).toString(),
-          })
-        }
+          });
+        }}
       />
     </div>
     <div className="col-lg-6 mb-2">
       <p class="mb-1">
-        {isSponsorship ? "Sponsor" : "Requested sponsor"}
+        Requested sponsor
         <span class="text-muted fw-normal">(Optional)</span>
       </p>
       <p style={{ fontSize: "13px" }} class="m-0 text-muted fw-light">
@@ -491,9 +528,7 @@ const fundraisingDiv = (
 );
 
 function generateDescription(text, amount, token, supervisor) {
-  const funding = isSponsorship
-    ? `###### Sponsored amount: ${amount} ${token}\n###### Sponsor: @${supervisor}\n`
-    : `###### Requested amount: ${amount} ${token}\n###### Requested sponsor: @${supervisor}\n`;
+  const funding = `###### Requested amount: ${amount} ${token}\n###### Requested sponsor: @${supervisor}\n`;
   if (amount > 0 && token && supervisor) return funding + text;
   return text;
 }
@@ -527,24 +562,21 @@ return (
       where the title renders extra on state change. */}
       {fields.includes("githubLink") ? (
         <div className="row">
-          {githubLinkDiv}
+          {fields.includes("githubLink") && githubLinkDiv}
           {labelEditor}
           {fields.includes("name") && nameDiv}
-          {fields.includes("description") && descriptionDiv}
-          {fields.includes("fund_raising") && isFundraisingDiv}
-          {state.seekingFunding &&
-            fields.includes("fund_raising") &&
-            fundraisingDiv}
+          {fields.includes("description") && callDescriptionDiv()}
         </div>
       ) : (
         <div className="row">
           {labelEditor}
           {fields.includes("name") && nameDiv}
-          {fields.includes("description") && descriptionDiv}
+          {fields.includes("amount") && amountDiv}
+          {fields.includes("sponsorship_token") && tokenDiv}
+          {fields.includes("supervisor") && supervisorDiv}
+          {fields.includes("description") && callDescriptionDiv()}
           {fields.includes("fund_raising") && isFundraisingDiv}
-          {state.seekingFunding &&
-            fields.includes("fund_raising") &&
-            fundraisingDiv}
+          {state.seekingFunding && fundraisingDiv}
         </div>
       )}
 
@@ -566,12 +598,14 @@ return (
             labels: state.labelStrings,
             post_type: postType,
             name: state.name,
-            description: generateDescription(
+            description: 
+            postType == 'Submission' ?
+            generateDescription(
               state.description,
               state.amount,
               state.token,
               state.supervisor
-            ),
+            ): state.description,
             amount: state.amount,
             sponsorship_token: state.token,
             supervisor: state.supervisor,
