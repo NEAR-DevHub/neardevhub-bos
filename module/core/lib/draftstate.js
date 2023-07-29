25d24a89ccc57f316bad811af96d8742a234a372
Peter Salomonsen <petersalomonsen@runbox.com>
2023-07-29 17:01:19 +0000

    fix: confirmation only for add/edit post
    check the method name of the returned transaction
    only show the post confirmation screen if it is edit_post or add_post
    
    fixes #209

--- module/core/lib/draftstate.js
+++ module/core/lib/draftstate.js
@@ -1,6 +1,27 @@
 const DRAFT_STATE_STORAGE_KEY = "POST_DRAFT_STATE";
+let is_edit_or_add_post_transaction = false;
 if (props.transactionHashes) {
-  Storage.privateSet(DRAFT_STATE_STORAGE_KEY, undefined);
+  const transaction = fetch("https://rpc.mainnet.near.org", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "dontcare",
+      method: "tx",
+      params: [props.transactionHashes, context.accountId],
+    }),
+  });
+  const method_name =
+    transaction?.body?.result?.transaction?.actions[0].FunctionCall.method_name;
+
+  is_edit_or_add_post_transaction =
+    method_name == "add_post" || method_name == "edit_post";
+
+  if (is_edit_or_add_post_transaction) {
+    Storage.privateSet(DRAFT_STATE_STORAGE_KEY, undefined);
+  }
 }
 
 const onDraftStateChange = (draftState) =>
