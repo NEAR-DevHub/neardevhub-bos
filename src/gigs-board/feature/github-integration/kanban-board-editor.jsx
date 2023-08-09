5d137be6cc0668e72019c072585cb188457167c3
Carina.Akaia.near <carina@dmail.ai>
2023-08-10 02:27:36 +0400

    Add viewer's project permissions checker

--- src/gigs-board/feature/github-integration/kanban-board-editor.jsx
+++ src/gigs-board/feature/github-integration/kanban-board-editor.jsx
@@ -344,6 +344,19 @@ const Viewer = {
         Viewer.role.isDevHubModerator),
   },
 
+  projectPermissions: (projectId) => {
+    const project_id = parseInt(projectId);
+
+    const defaultPermissions = { can_configure: false };
+
+    return !isNaN(project_id)
+      ? Near.view(devHubAccountId, "get_account_project_permissions", {
+          account_id: context.accountId,
+          project_id: project_id,
+        }) ?? defaultPermissions
+      : defaultPermissions;
+  },
+
   role: {
     isDevHubModerator:
       access_control_info.data === null || access_control_info.isLoading
