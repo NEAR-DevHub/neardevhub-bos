346bf98014d3d5ac523b4bd7fcfefe527760b0d2
Carina.Akaia.near <carina@dmail.ai>
2023-08-16 21:48:19 +0400

    hotfix: communities creation and updating compatibility issue after deploying the contract upgrades too soon (#244)

--- src/gigs-board/entity/community/header.jsx
+++ src/gigs-board/entity/community/header.jsx
@@ -108,36 +108,9 @@ const devHubAccountId =
   (context.widgetSrc ?? "devgovgigs.near").split("/", 1)[0];
 
 const DevHub = {
-  edit_community_github: ({ handle, github }) =>
-    Near.call(devHubAccountId, "edit_community_github", { handle, github }) ??
+  update_community_github: ({ handle, github }) =>
+    Near.call(devHubAccountId, "update_community_github", { handle, github }) ??
     null,
-
-  create_workspace: ({ author_community_handle, metadata }) =>
-    Near.call(devHubAccountId, "create_workspace", {
-      author_community_handle,
-      metadata,
-    }) ?? null,
-
-  delete_workspace: ({ id }) =>
-    Near.call(devHubAccountId, "delete_workspace", { id }) ?? null,
-
-  update_workspace_metadata: ({ metadata }) =>
-    Near.call(devHubAccountId, "update_workspace_metadata", { metadata }) ??
-    null,
-
-  get_workspace_views_metadata: ({ workspace_id }) =>
-    Near.view(devHubAccountId, "get_workspace_views_metadata", {
-      workspace_id,
-    }) ?? null,
-
-  create_workspace_view: ({ view }) =>
-    Near.call(devHubAccountId, "create_workspace_view", { view }) ?? null,
-
-  update_workspace_view: ({ view }) =>
-    Near.call(devHubAccountId, "update_workspace_view", { view }) ?? null,
-
-  delete_workspace_view: ({ id }) =>
-    Near.call(devHubAccountId, "delete_workspace_view", { id }) ?? null,
 
   get_access_control_info: () =>
     Near.view(devHubAccountId, "get_access_control_info") ?? null,
@@ -145,7 +118,7 @@ const DevHub = {
   get_all_authors: () => Near.view(devHubAccountId, "get_all_authors") ?? null,
 
   get_all_communities: () =>
-    Near.view(devHubAccountId, "get_all_communities") ?? null,
+    Near.view(devHubAccountId, "get_all_communities_metadata") ?? null,
 
   get_all_labels: () => Near.view(devHubAccountId, "get_all_labels") ?? null,
 
