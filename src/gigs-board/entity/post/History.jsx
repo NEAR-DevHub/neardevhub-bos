6ce0739408bc8ba9398bb05905cab41272a7ab76
Carina.Akaia.near <carina@dmail.ai>
2023-10-13 11:01:45 +0400

    wip: Prepare branch for release

--- src/gigs-board/entity/post/History.jsx
+++ src/gigs-board/entity/post/History.jsx
@@ -68,7 +68,7 @@ const post =
     post_id: postId,
   });
 if (!post || !post.snapshot_history) {
-  return <div class="bi bi-clock-history px-2"></div>;
+  return <div className="bi bi-clock-history px-2"></div>;
 }
 const referral = props.referral;
 
@@ -91,20 +91,20 @@ snapshotHistory.push(snapshot);
 snapshotHistory.reverse();
 
 const history = (
-  <div class="btn-group" role="group">
+  <div className="btn-group" role="group">
     <a
-      class="card-link"
+      className="card-link"
       role="button"
       title="Post History"
       data-bs-toggle="dropdown"
       aria-expanded="false"
       type="button"
     >
-      <div class="bi bi-clock-history px-2"></div>
+      <div className="bi bi-clock-history px-2"></div>
     </a>
-    <ul class="dropdown-menu">
+    <ul className="dropdown-menu">
       <a
-        class="d-flex text-muted"
+        className="d-flex text-muted"
         style={{ fontSize: "12px", textDecoration: "none", cursor: "default" }}
       >
         <a
@@ -129,7 +129,7 @@ const history = (
               }}
             >
               <a
-                class="dropdown-item"
+                className="dropdown-item"
                 href={href("Post", {
                   id: postId,
                   timestamp: item.timestamp,
@@ -157,7 +157,7 @@ const history = (
               </a>
             </div>
             <a
-              class="dropdown-item"
+              className="dropdown-item"
               href={href("Post", {
                 id: postId,
                 timestamp: currentTimestamp,
@@ -165,7 +165,7 @@ const history = (
                 referral,
               })}
             >
-              <i class="bi bi-file-earmark-diff" />
+              <i className="bi bi-file-earmark-diff" />
             </a>
           </li>
         );
