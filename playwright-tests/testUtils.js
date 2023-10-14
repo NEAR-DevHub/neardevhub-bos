f34862d251b781ca05696bbea2c0e2a38f7501b9
Espen Simensen <espen@kodetid.no>
2023-10-14 13:42:04 +0200

    test: Added test for creating a solution post

---
+++ playwright-tests/testUtils.js
@@ -0,0 +1,19 @@
+import { expect } from "@playwright/test";
+
+export const setInputAndAssert = async (page, selector, value) => {
+  await page.fill(selector, value);
+  const actualValue = await page.inputValue(selector);
+  expect(actualValue).toBe(value);
+}
+
+export const selectAndAssert = async (page, selector, value) => {
+  await page.selectOption(selector, { value: value });
+  const selectedValue = await page.$eval(selector, (select) => select.value);
+  expect(selectedValue).toBe(value);
+}
+
+export const waitForSelectorToBeVisible = async (page, selector) => {
+  await page.waitForSelector(selector, {
+    state: "visible",
+  });
+}
