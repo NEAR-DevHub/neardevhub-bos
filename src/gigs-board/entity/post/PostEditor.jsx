2aa6f31be5623703b13e2dcf3e35bcacfeddcb90
Espen Simensen <espen@kodetid.no>
2023-10-18 07:46:04 +0200

    feat: Setting correct usdc address

--- src/gigs-board/entity/post/PostEditor.jsx
+++ src/gigs-board/entity/post/PostEditor.jsx
@@ -142,7 +142,7 @@ const tokenMapping = {
   },
   USDC: {
     NEP141: {
-      address: "usdc.tether-token.near",
+      address: "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
     },
   },
 };
