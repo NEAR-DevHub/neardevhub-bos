5862634a4c1a388b6f09ba825b5f7be06ef538a9
Espen Simensen <espen@kodetid.no>
2023-10-25 07:38:36 +0200

    fix: prettier formatting

--- src/gigs-board/entity/post/PostEditor.jsx
+++ src/gigs-board/entity/post/PostEditor.jsx
@@ -142,7 +142,8 @@ const tokenMapping = {
   },
   USDC: {
     NEP141: {
-      address: "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
+      address:
+        "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
     },
   },
 };
