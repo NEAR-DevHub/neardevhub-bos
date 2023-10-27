78a78a1f9710cecc2c566525557207d982e479e1
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-10-27 02:59:38 -0400

    feat: improves wiki plugin

--- src/devhub/entity/addon/wiki/Viewer.jsx
+++ src/devhub/entity/addon/wiki/Viewer.jsx
@@ -1,27 +1,25 @@
-const { title, description, content } = props;
+const { content, title, description, textAlign } = props;
 
 const Container = styled.div`
-  width: 80%;
+  width: 100%;
   margin: 0 auto;
   padding: 20px;
-  text-align: center;
-`;
-
-const Title = styled.h1`
-  font-size: 24px;
-  margin: 10px 0;
-`;
-
-const Description = styled.p`
-  font-size: 16px;
-  color: #555;
-  margin: 10px 0;
+  text-align: ${(p) => p.textAlign ?? "left"};
 `;
 
 const Content = styled.div`
   margin: 20px 0;
   text-align: left;
 `;
+
+const Title = styled.h1`
+  margin-bottom: 10px; /* Optional: Adjust margin as needed */
+`;
+
+const Description = styled.p`
+  margin-bottom: 20px; /* Optional: Adjust margin as needed */
+`;
+
 const CenteredMessage = styled.div`
   display: flex;
   flex-direction: column;
@@ -31,7 +29,7 @@ const CenteredMessage = styled.div`
   height: ${(p) => p.height ?? "100%"};
 `;
 
-if (!title || !content) {
+if (!content) {
   return (
     <CenteredMessage height={"384px"}>
       <h2>No Wiki Configured</h2>
@@ -39,7 +37,7 @@ if (!title || !content) {
   );
 } else {
   return (
-    <Container>
+    <Container textAlign={textAlign}>
       <Title>{title}</Title>
       <Description>{description}</Description>
       <Content>
