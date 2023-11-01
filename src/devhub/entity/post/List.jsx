06dec63350a4f8cdd22d504cca8048f35a0370a5
Peter Salomonsen <petersalomonsen@runbox.com>
2023-11-01 23:54:50 +0100

    Fix/376 blog posts should not appear in activity feeds (#400)
    * fix: migrate edd96d0d3c9 to new refactor
    
    * Ensure that 36750eb315f1e686e migrated to new refactor
    
    * fix test
    
    * fix: hide blog in activity feeds
    
    fixes #376
    
    * prettier
    
    * include all criterias for a blog to hide
    
    * no changes in configurator
    
    ---------
    
    Co-authored-by: Thomas <thomas@guntenaar.org>
    Co-authored-by: Elliot Braem <16282460+elliotBraem@users.noreply.github.com>

--- src/devhub/entity/post/List.jsx
+++ src/devhub/entity/post/List.jsx
@@ -126,6 +126,16 @@ function getPostIds() {
     where = { parent_id: { _is_null: true }, ...where };
   }
 
+  // Don't show blog
+  where = {
+    _not: {
+      labels: { _contains: "blog" },
+      parent_id: { _is_null: true },
+      post_type: { _eq: "Comment" },
+    },
+    ...where,
+  };
+
   console.log("searching for", where);
   fetchGraphQL(query, "DevhubPostsQuery", {
     limit: 100,
