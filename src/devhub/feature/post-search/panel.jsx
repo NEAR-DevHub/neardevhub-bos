ba70894c3da9ce36a9014f83d7db7ab4b740afde
Zeeshan Ahmad <itexpert120@outlook.com>
2023-11-02 13:33:08 +0500

    Responsive Feed controls

--- src/devhub/feature/post-search/panel.jsx
+++ src/devhub/feature/post-search/panel.jsx
@@ -32,86 +32,100 @@ const PageTitle = styled.h5`
 
 const Container = styled.div`
   padding: 24px;
+  width: 100%;
 `;
 
 const PostContainer = styled.div`
   margin: 0 1rem;
 `;
 
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  width: 25%;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
 return (
   <>
     {!props.hideHeader && (
       <Container>
-        <div>
+        <div className="w-100">
           <PageTitle>Activity Feed</PageTitle>
-          <div className="d-flex flex-row gap-4">
-            <div className="d-flex flex-row position-relative w-25">
-              <div className="position-absolute d-flex ps-3 flex-column h-100 justify-center">
-                <i class="bi bi-search m-auto"></i>
+          <div>
+            <div className="d-flex flex-column flex-lg-row gap-4">
+              <InputContainer>
+                <div className="position-absolute d-flex ps-3 flex-column h-100 justify-center">
+                  <i class="bi bi-search m-auto"></i>
+                </div>
+                <input
+                  type="search"
+                  className="ps-5 form-control border border-0 bg-light"
+                  value={state.term ?? ""}
+                  onChange={(e) => updateInput(e.target.value)}
+                  onKeyDown={(e) => e.key == "Enter" && search()}
+                  placeholder={props.placeholder ?? `Search by content`}
+                />
+              </InputContainer>
+              <div class="dropdown">
+                <button
+                  class="btn btn-light dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Sort{props.recency === "all" ? ": All replies" : ": Latest"}
+                </button>
+                <ul class="dropdown-menu px-2 shadow">
+                  <li>
+                    <a
+                      style={{ borderRadius: "5px" }}
+                      class="dropdown-item link-underline link-underline-opacity-0"
+                      href={href("Feed")}
+                    >
+                      Latest
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      style={{ borderRadius: "5px" }}
+                      class="dropdown-item link-underline link-underline-opacity-0"
+                      href={href("Feed", { recency: "all" })}
+                    >
+                      All replies
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <input
-                type="search"
-                className="ps-5 form-control border border-0 bg-light"
-                value={state.term ?? ""}
-                onChange={(e) => updateInput(e.target.value)}
-                onKeyDown={(e) => e.key == "Enter" && search()}
-                placeholder={props.placeholder ?? `Search by content`}
-              />
-            </div>
-            <div class="dropdown">
-              <button
-                class="btn btn-light dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Sort{props.recency === "all" ? ": All replies" : ": Latest"}
-              </button>
-              <ul class="dropdown-menu px-2 shadow">
-                <li>
-                  <a
-                    style={{ borderRadius: "5px" }}
-                    class="dropdown-item link-underline link-underline-opacity-0"
-                    href={href("Feed")}
-                  >
-                    Latest
-                  </a>
-                </li>
-                <li>
-                  <a
-                    style={{ borderRadius: "5px" }}
-                    class="dropdown-item link-underline link-underline-opacity-0"
-                    href={href("Feed", { recency: "all" })}
-                  >
-                    All replies
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="dropdown">
-              <Widget
-                src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-author"
-                props={{
-                  author: state.author,
-                  onAuthorSearch: (author) => {
-                    State.update({ author });
-                  },
-                }}
-              />
-            </div>
-            <div>
-              <Widget
-                src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-tag"
-                props={{
-                  tag: state.tag,
-                  onTagSearch: (tag) => {
-                    State.update({ tag });
-                  },
-                }}
-              />
-            </div>
-            <div className="d-flex flex-row-reverse flex-grow-1">
-              {props.children}
+              <div class="dropdown">
+                <Widget
+                  src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-author"
+                  props={{
+                    author: state.author,
+                    onAuthorSearch: (author) => {
+                      State.update({ author });
+                    },
+                  }}
+                />
+              </div>
+              <div>
+                <Widget
+                  src="${REPL_DEVHUB}/widget/devhub.feature.post-search.by-tag"
+                  props={{
+                    tag: state.tag,
+                    onTagSearch: (tag) => {
+                      State.update({ tag });
+                    },
+                  }}
+                />
+              </div>
+              <div className="d-flex flex-row-reverse flex-grow-1">
+                {props.children}
+              </div>
             </div>
           </div>
         </div>
