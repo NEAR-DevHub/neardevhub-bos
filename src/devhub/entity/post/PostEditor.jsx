420ca4f90e53bef3250f3715c9c550a1e9dc7d1f
Carina.Akaia.near <carina@dmail.ai>
2023-11-03 18:41:44 +0400

    Use updated post version key for solutions (#426)
    * chore: Format
    
    * Set updated post type for solutions
    
    * Use updated post version key for solutions
    
    * fix: Enable accepting cookie consent in tests
    
    * chore: Format
    
    * chore: Trigger test pipeline retry

--- src/devhub/entity/post/PostEditor.jsx
+++ src/devhub/entity/post/PostEditor.jsx
@@ -36,6 +36,7 @@ const mode = props.mode ?? "Create";
 
 const referralLabels = props.referral ? [`referral:${props.referral}`] : [];
 const labelStrings = (props.labels ?? []).concat(referralLabels);
+
 const labels = labelStrings.map((s) => {
   return { name: s };
 });
@@ -121,7 +122,7 @@ const onSubmit = () => {
         state.supervisor,
         state.seekingFunding
       ),
-      submission_version: "V1",
+      solution_version: "V1",
     },
     Attestation: {
       name: state.name,
