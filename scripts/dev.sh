0fef9cc214167fd7712075bcfe4a49cefa24dd4a
Thomas <thomas@guntenaar.org>
2023-11-16 17:12:09 +0200

    wip: move replacements to js script and out of dev.sh (mac only) and out of github action

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,13 +5,6 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-# Check if jq is installed
-if ! command_exists jq; then
-  echo "jq is not installed. Installing..."
-  sudo apt-get update
-  sudo apt-get install -y jq
-fi
-
 # Check if bos-loader is installed
 if ! command_exists bos-loader; then
     # Install bos-loader
@@ -19,31 +12,13 @@ if ! command_exists bos-loader; then
     curl --proto '=https' --tlsv1.2 -LsSf https://github.com/mpeterdev/bos-loader/releases/download/v0.7.1/bos-loader-v0.7.1-installer.sh | sh
 fi
 
-# Define default values
-NETWORK_ENV="testnet"
-CREATOR_REPL="REPL_DEVHUB"
-CONTRACT_REPL="REPL_DEVHUB_CONTRACT"
-REPLACE_ACCOUNT=false
-REPLACE_CONTRACT=false
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
         -a|--account)
-            ACCOUNT_ID="$2"
-            REPLACE_ACCOUNT=true
-            shift
-            shift
-            ;;
-        -c|--contract)
-            CONTRACT_ID="$2"
-            REPLACE_CONTRACT=true
-            shift
-            shift
-            ;;
-        -n|--network)
-            NETWORK_ENV="$2"
+            ACCOUNT_ID="$2"   
             shift
             shift
             ;;
@@ -53,36 +28,6 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
-
-# Convert NETWORK_ENV to lowercase
-NETWORK_ENV=$(echo "$NETWORK_ENV" | tr '[:upper:]' '[:lower:]')
-
-echo "NETWORK_ENV: $NETWORK_ENV"
-
-# Update the value in replacements.json
-REPLACEMENTS_JSON="replacements.$NETWORK_ENV.json"
-
-if [ -f "$REPLACEMENTS_JSON" ]; then
-    # Copy the content of the original JSON file to a temp one
-    cp "$REPLACEMENTS_JSON" "$REPLACEMENTS_JSON.tmp"
-
-    # If ACCOUNT_ID is not provided as an argument, get it from the replacements file
-    if [ "$REPLACE_ACCOUNT" != true ]; then
-        ACCOUNT_ID=$(jq -r ".[\"$CREATOR_REPL\"]" "$REPLACEMENTS_JSON")
-    fi
-
-    # Replace the value in the JSON file only if the arguments are provided
-    if [ "$REPLACE_ACCOUNT" = true ]; then
-        jq --arg ACCOUNT_ID "$ACCOUNT_ID" ".[\"$CREATOR_REPL\"] = \"$ACCOUNT_ID\"" "$REPLACEMENTS_JSON.tmp" > temp.json && mv temp.json "$REPLACEMENTS_JSON.tmp"
-    fi
-
-    if [ "$REPLACE_CONTRACT" = true ]; then
-        jq --arg CONTRACT_ID "$CONTRACT_ID" ".[\"$CONTRACT_REPL\"] = \"$CONTRACT_ID\"" "$REPLACEMENTS_JSON.tmp" > temp.json && mv temp.json "$REPLACEMENTS_JSON.tmp"
-    fi
-else
-    echo "Error: $REPLACEMENTS_JSON file not found."
-    exit 1
-fi
 
 # Run bos-loader with updated replacements
 ~/.cargo/bin/bos-loader "$ACCOUNT_ID" --path build/src
---
+++ scripts/replace.js
@@ -0,0 +1,134 @@
+import fs from "fs";
+import path from "path";
+
+const args = process.argv.slice(2);
+
+let NETWORK_ENV = "testnet";
+let CREATOR_REPL = "REPL_DEVHUB";
+let CONTRACT_REPL = "REPL_DEVHUB_CONTRACT";
+let ACCOUNT_ID = "";
+let SIGNER_ID = "";
+let CONTRACT_ID = "";
+
+// Parse command line arguments
+for (let i = 0; i < args.length; i++) {
+  switch (args[i]) {
+    case "-a":
+    case "--account":
+      ACCOUNT_ID = args[++i];
+      break;
+    case "-s":
+    case "--signer":
+      SIGNER_ID = args[++i];
+      break;
+    case "-c":
+    case "--contract":
+      CONTRACT_ID = args[++i];
+      break;
+    case "-n":
+    case "--network":
+      NETWORK_ENV = args[++i].toLowerCase();
+      break;
+    default:
+      console.error(`Unknown option: ${args[i]}`);
+      process.exit(1);
+  }
+}
+
+console.log(`NETWORK_ENV: ${NETWORK_ENV}`);
+
+// Check if account is provided
+if (!ACCOUNT_ID) {
+  console.error(
+    "Error: Account is not provided. Please provide the account to deploy the widgets to."
+  );
+  process.exit(1);
+}
+
+if (!SIGNER_ID) {
+  SIGNER_ID = ACCOUNT_ID;
+}
+
+// Check if network is provided but not contract
+if (NETWORK_ENV === "testnet" && !CONTRACT_ID) {
+  console.error(
+    "Error: Network is set to testnet but no contract is provided. Please specify a contract to use with testnet."
+  );
+  process.exit(1);
+}
+
+// Update the value in replacements.json
+let REPLACEMENTS_JSON = `replacements.${NETWORK_ENV}.json`;
+
+if (fs.existsSync(REPLACEMENTS_JSON)) {
+  // Replace the value in the JSON file
+  let replacements = JSON.parse(fs.readFileSync(REPLACEMENTS_JSON, "utf8"));
+  replacements[CREATOR_REPL] = ACCOUNT_ID;
+  replacements[CONTRACT_REPL] = CONTRACT_ID;
+  fs.writeFileSync(
+    `${REPLACEMENTS_JSON}.tmp`,
+    JSON.stringify(replacements, null, 2)
+  );
+} else {
+  console.error(`Error: ${REPLACEMENTS_JSON} file not found.`);
+  process.exit(1);
+}
+
+// Read the content of the .tmp file
+let replacements = JSON.parse(
+  fs.readFileSync(`${REPLACEMENTS_JSON}.tmp`, "utf8")
+);
+
+// Extract all keys (placeholders to be replaced)
+let keys = Object.keys(replacements);
+
+// Recursively get all .jsx files
+function getAllFiles(dirPath, arrayOfFiles) {
+  let files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+let FILES = getAllFiles("build/src").filter((file) => file.endsWith(".jsx"));
+
+console.log("Building widgets...");
+
+// Iterate over each .jsx file
+FILES.forEach((file) => {
+  // Create corresponding directory structure in build/src folder
+  let dir = path.dirname(file);
+  if (!fs.existsSync(`build/${dir}`)) {
+    fs.mkdirSync(`build/${dir}`, { recursive: true });
+  }
+
+  // Define the output path
+  let outfile = `build/${file}`;
+  fs.copyFileSync(file, outfile); // initialize outfile with the original file content
+});
+
+console.log("Making replacements...");
+
+// Iterate over each .jsx file again for replacements
+FILES.forEach((file) => {
+  let outfile = `build/${file}`;
+  console.log(outfile);
+
+  let content = fs.readFileSync(outfile, "utf8");
+  // Iterate over each key to get the replacement value
+  keys.forEach((key) => {
+    let replace = replacements[key];
+    let search = new RegExp(`\\$\\{${key}\\}`, "g");
+    content = content.replace(search, replace);
+  });
+  fs.writeFileSync(outfile, content);
+});
