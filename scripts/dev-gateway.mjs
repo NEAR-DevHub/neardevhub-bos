61d8515c9b325b3aebbf3fec09d8014aea87762b
Thomas <thomas@guntenaar.org>
2023-11-19 17:44:11 +0200

    fix: pipeline test issue caused by watchman

--- scripts/dev-gateway.mjs
+++ scripts/dev-gateway.mjs
@@ -1,20 +1,20 @@
 // startServer.mjs
-import httpServer from 'http-server';
-import path from 'path';
-import { spawn } from 'child_process';
+import httpServer from "http-server";
+import path from "path";
+import { spawn } from "child_process";
 
 // Start the HTTP server
 const server = httpServer.createServer({
-  root: path.join(process.cwd(), 'node_modules/near-bos-webcomponent/dist/')
+  root: path.join(process.cwd(), "node_modules/near-bos-webcomponent/dist/"),
 });
 
 server.listen(8080, () => {
-  console.log('HTTP server is listening on port 8080');
+  console.log("HTTP server is listening on port 8080");
 });
 
 // Start the 'npm run dev' process
-const npmRunDev = spawn('npm', ['run', 'dev'], { stdio: 'inherit' });
+const npmRunDev = spawn("npm", ["run", "dev:mainnet"], { stdio: "inherit" });
 
-npmRunDev.on('close', (code) => {
+npmRunDev.on("close", (code) => {
   console.log(`npm run dev process exited with code ${code}`);
 });
