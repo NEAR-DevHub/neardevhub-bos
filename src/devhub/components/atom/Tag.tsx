8401e47e48da773ccb1afebb26d6d0a7c9eb7e80
Thomas <thomas@guntenaar.org>
2023-11-19 17:18:53 +0200

    feat: add watchman to build files on change

--- src/devhub/components/atom/Tag.tsx
+++ src/devhub/components/atom/Tag.tsx
@@ -4,23 +4,6 @@ type TagProps = {
 };
 
 export default function Tag(props: TagProps) {
-  // TODO remove
-  const REPL_DEVHUB_CONTRACT = "${REPL_DEVHUB_CONTRACT}";
-  const REPL_DEVHUB_LEGACY = "${REPL_DEVHUB_LEGACY}";
-  const REPL_DEVHUB = "${REPL_DEVHUB}";
-  const REPL_NEAR = "${REPL_NEAR}";
-  const REPL_MOB = "${REPL_MOB}";
-  const REPL_EFIZ = "${REPL_EFIZ}";
-
-  console.log({
-    REPL_DEVHUB_CONTRACT,
-    REPL_DEVHUB_LEGACY,
-    REPL_DEVHUB,
-    REPL_NEAR,
-    REPL_MOB,
-    REPL_EFIZ,
-  });
-
   const { black, tag } = props;
 
   const Span = styled.span`
