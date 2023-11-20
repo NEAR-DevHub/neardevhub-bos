6b331b8a14e12eb2bac13cf9b270e96189d4af28
Thomas <thomas@guntenaar.org>
2023-11-20 13:13:48 +0200

    remove old comments

--- scripts/build.js
+++ scripts/build.js
@@ -4,13 +4,6 @@ import replaceInFiles from "replace-in-files";
 const transpiledPathPrefix = "./build/src";
 
 async function build() {
-  // await replaceInFiles({
-  //   files: [`${transpiledPathPrefix}/**/*.jsx`],
-  //   from: /export\s+default\s+function[^(]*\((.*)/gms,
-  //   to: (_match, rest) =>
-  //     `function MainComponent(${rest}\nreturn MainComponent(props, context);`,
-  // });
-  // TODO test
   await replaceInFiles({
     files: [`${transpiledPathPrefix}/**/*.jsx`],
     from: /export\s+default\s+function\s+(\w+)\((.*)/gms,
