8ad6eb476608c36e55b5c04666e331591b533b46
Thomas <thomas@guntenaar.org>
2023-11-22 10:15:36 +0200

    fix: removed cause generated neardevhub-widgets

--- scripts/build.js
+++ scripts/build.js
@@ -18,40 +18,6 @@ async function build() {
     to: () => "",
   });
 
-  // WARNING: Don't allow "imports" in includes as this may lead to undefined
-  // behavior as replacements are done in parallel and one file may be getting
-  // replacements saved while the other file needs to include it, which ends up
-  // with empty content includes.
-  await new Promise((resolve) => {
-    fs.rename(
-      `${transpiledPathPrefix}/includes`,
-      `${transpiledPathPrefix}/../../includes`,
-      () => {
-        resolve();
-      }
-    );
-  });
-
-  const packageJson = JSON.parse(
-    fs.readFileSync(new URL("../package.json", import.meta.url))
-  );
-
-  // await replaceInFiles({
-  //   files: [`${transpiledPathPrefix}/**/*.jsx`],
-  //   from: /^/m,
-  //   to: `/*\nLicense: ${packageJson.license}\nAuthor: ${packageJson.author}\nHomepage: ${packageJson.homepage}\n*/\n`,
-  // });
-
-  await new Promise((resolve) => {
-    fs.rename(
-      transpiledPathPrefix,
-      `${transpiledPathPrefix}/../../${packageJson.name}`,
-      () => {
-        resolve();
-      }
-    );
-  });
-
   console.log("DONE");
 }
 
