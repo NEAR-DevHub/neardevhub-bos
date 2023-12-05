91241041d397329825907dbe603086f1bad7d8b8
Thomas <thomasguntenaar@gmail.com>
2023-12-05 14:58:25 +0200

    changed: use watcher instead of fb-watchman

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,77 +1,19 @@
 import { spawn } from "child_process";
-import watchman from "fb-watchman";
-var client = new watchman.Client();
+import Watcher from "watcher";
 
 var dir_of_interest = process.cwd() + "/src";
 
-client.capabilityCheck(
-  { optional: [], required: ["relative_root"] },
-  function (error, resp) {
-    if (error) {
-      console.log(error);
-      client.end();
-      return;
-    }
+// Passing an "all" handler directly
+const watcher = new Watcher(
+  dir_of_interest,
+  {}, // passing some options
+  (event, targetPath, targetPathNext) => {
+    // This is what the library does internally when you pass it a handler directly
+    console.log(event); // => could be any target event: 'add', 'addDir', 'change', 'rename', 'renameDir', 'unlink' or 'unlinkDir'
+    console.log(targetPath); // => the file system path where the event took place, this is always provided
+    console.log(targetPathNext); // => the file system path "targetPath" got renamed to, this is only provided on 'rename'/'renameDir' events
 
-    // Initiate the watch
-    client.command(["watch-project", dir_of_interest], function (error, resp) {
-      if (error) {
-        console.error("Error initiating watch:", error);
-        const npmClearWatch = spawn("npm", ["run", "clear:watch"], {
-          stdio: "inherit",
-        });
-
-        // Run watch again
-        npmClearWatch.on("close", () => {
-          console.log("Try and re-run the npm run watch command.");
-        });
-
-        return;
-      }
-
-      // It is considered to be best practice to show any 'warning' or
-      // 'error' information to the user, as it may suggest steps
-      // for remediation
-      if ("warning" in resp) {
-        console.log("warning: ", resp.warning);
-      }
-
-      console.log("watch established on ", resp.watch);
-
-      make_subscription(client, resp.watch, resp.relative_path);
-    });
-  }
-);
-
-function make_subscription(client, watch, relative_path) {
-  const sub = {
-    // Match any `.jsx` or `.tsx` files in the src directory
-    expression: ["anyof", ["match", "*.jsx"], ["match", "*.tsx"]],
-    // Which fields we're interested in
-    fields: ["name", "size", "mtime_ms", "exists", "type"],
-  };
-  if (relative_path) {
-    sub.relative_root = relative_path;
-  }
-
-  client.command(
-    ["subscribe", watch, "mysubscription", sub],
-    function (error, resp) {
-      if (error) {
-        // Probably an error in the subscription criteria
-        console.error("failed to subscribe: ", error);
-        return;
-      }
-      console.log("subscription " + resp.subscribe + " established");
-    }
-  );
-
-  client.on("subscription", function (resp) {
-    if (resp.subscription !== "mysubscription") return;
-
-    resp.files.forEach(function (file) {
-      console.log("file changed: " + file.name);
-    });
+    console.log("file changed: " + targetPath);
     console.log(`Building...`);
 
     // Start the 'npm run build' process
@@ -80,5 +22,32 @@ function make_subscription(client, watch, relative_path) {
     npmRunBuild.on("close", (code) => {
       console.log(`Build finished`);
     });
-  });
-}
+  }
+);
+
+watcher.on("error", (error) => {
+  console.log(error instanceof Error); // => true, "Error" instances are always provided on "error"
+});
+
+watcher.on("ready", () => {
+  // The app just finished instantiation and may soon emit some events
+});
+watcher.on("close", () => {
+  // The app just stopped watching and will not emit any further events
+});
+
+watcher.on("add", (filePath) => {
+  console.log(filePath); // "filePath" just got created, or discovered by the watcher if this is an initial event
+});
+watcher.on("addDir", (directoryPath) => {
+  console.log(directoryPath); // "directoryPath" just got created, or discovered by the watcher if this is an initial event
+});
+watcher.on("change", (filePath) => {
+  console.log(filePath); // "filePath" just got modified
+});
+watcher.on("rename", (filePath, filePathNext) => {
+  console.log(filePath, filePathNext); // "filePath" got renamed to "filePathNext"
+});
+watcher.on("renameDir", (directoryPath, directoryPathNext) => {
+  console.log(directoryPath, directoryPathNext); // "directoryPath" got renamed to "directoryPathNext"
+});
