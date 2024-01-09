6fcc8304c4b9888b7b220756054d82528d052341
Megha-Dev-19 <meghadev1901@gmail.com>
2024-01-09 15:49:48 +0530

    treasure UI update

--- src/devhub/entity/trustee/treasury.jsx
+++ src/devhub/entity/trustee/treasury.jsx
@@ -18,6 +18,10 @@ const Container = styled.div`
     font-size: 12px;
     font-weight: 400;
   }
+
+  .gap-10 {
+    gap: 5rem;
+  }
 `;
 
 function convertYoctoToNear(yoctoNear) {
@@ -34,8 +38,11 @@ function convertYoctoToNear(yoctoNear) {
 
 const accountId = "devhub.near";
 const res = fetch(`https://api.nearblocks.io/v1/account/${accountId}`);
+const txns = fetch(
+  `https://api.nearblocks.io/v1/account/${accountId}/ft-txns/count`
+);
 
-if (res === null) {
+if (res === null || txns === null) {
   return <></>;
 }
 
@@ -50,7 +57,7 @@ return (
           onClick={() => clipboard.writeText(accountId)}
         ></i>
       </div>
-      <div className="flex-item d-flex gap-4 text-small">
+      <div className="flex-item d-flex gap-10 text-small">
         <div>
           <p>Balance</p>
           <p className="h5 bold">
@@ -65,8 +72,14 @@ return (
         </div>
         <div>
           <p>Transactions</p>
-          <p className="h5 bold">{}</p>
+          <p className="h5 bold">{txns?.body?.txns?.[0]?.count}</p>
         </div>
+      </div>
+      <div>
+        <img
+          src="https://ipfs.near.social/ipfs/bafkreicprzqio2e42dd5pk2cklqdxkwprukwac236air7gfxez6rgl57y4"
+          height={25}
+        />
       </div>
     </div>
   </Container>
