d0ec71cce7ddce54d7c2a5cf72cfe7bfbdae1262
Megha-Dev-19 <meghadev1901@gmail.com>
2024-01-30 17:28:02 +0530

    make app full width

--- src/devhub/components/templates/AppLayout.jsx
+++ src/devhub/components/templates/AppLayout.jsx
@@ -1,13 +1,16 @@
-const Container = styled.div`
+const Theme = styled.div`
+  position: fixed;
+  inset: 73px 0px 0px;
+  width: 100%;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100%;
-  min-height: 100vh;
-
+  overflow-y: scroll;
+  padding-top: calc(-1 * var(--body-top-padding));
   background: #f4f4f4;
+`;
 
-  margin-top: calc(-1 * var(--body-top-padding));
+const Container = styled.div`
+  width: 100%;
 `;
 
 const ContentContainer = styled.div`
@@ -41,13 +44,13 @@ const Footer = (props) => {
 
 function AppLayout({ page, children }) {
   return (
-    <>
+    <Theme>
       <Container>
         <AppHeader page={page} />
         <ContentContainer>{children}</ContentContainer>
         <Footer page={page} />
       </Container>
-    </>
+    </Theme>
   );
 }
 
