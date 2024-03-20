a2c4d46fc91f957404c4e1a89cd7a6cdc352b3e8
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-03-20 21:57:07 +0000

    fix error not being part of any test, because of active route handler. update to latest playwright to use page.unrouteAll function.

--- playwright-tests/tests/discussions.spec.js
+++ playwright-tests/tests/discussions.spec.js
@@ -305,5 +305,6 @@ test.describe("Don't ask again enabled", () => {
       .locator(".reposted")
       .waitFor({ state: "visible", timeout: 10000 });
     await pauseIfVideoRecording(page);
+    await page.unrouteAll();
   });
 });
