746347ffed0b1fb5e6a530f6ffac7c1dc7fb6324
Thomas <thomasguntenaar@gmail.com>
2024-05-11 14:14:07 +0200

    move tailwind higher up in the component hierarchy to encapsulate the entire blogv2 widget

--- src/devhub/entity/addon/blogv2/Configurator.jsx
+++ src/devhub/entity/addon/blogv2/Configurator.jsx
@@ -18,17 +18,37 @@ const Container = styled.div`
   }
 `;
 
+const css = fetch("https://floatui.com/tailwind.css").body;
+if (!css) return "";
+const Tailwind = styled.div`
+  ${css}
+
+  .bg-devhub-green {
+    background-color: #00ec97;
+  }
+
+  .bg-devhub-green-light {
+    background-color: rgb(212, 236, 227);
+  }
+
+  .text-devhub-green {
+    color: #00ec97;
+  }
+`;
+
 return (
-  <Tile className="p-3 bg-white min-vh-100">
-    <Container>
-      <Widget
-        src={"${REPL_DEVHUB}/widget/devhub.entity.addon.blogv2.editor.index"}
-        props={{
-          data,
-          handle,
-          communityAddonId,
-        }}
-      />
-    </Container>
-  </Tile>
+  <Tailwind>
+    <Tile className="p-3 bg-white min-vh-100">
+      <Container>
+        <Widget
+          src={"${REPL_DEVHUB}/widget/devhub.entity.addon.blogv2.editor.index"}
+          props={{
+            data,
+            handle,
+            communityAddonId,
+          }}
+        />
+      </Container>
+    </Tile>
+  </Tailwind>
 );
