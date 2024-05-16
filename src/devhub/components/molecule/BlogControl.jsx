55b187c62ce97cd81de607e09c5a866ad61aea24
Thomas <thomasguntenaar@gmail.com>
2024-05-16 18:34:24 +0200

    feat: added disabled property to BlogControl

--- src/devhub/components/molecule/BlogControl.jsx
+++ src/devhub/components/molecule/BlogControl.jsx
@@ -4,8 +4,11 @@ return (
   <button
     type="button"
     data-testid={props.testId ? props.testId : ""}
-    className="inline-flex items-center gap-x-1.5 rounded-md bg-devhub-green px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    className={`${
+      props.disabled ? "disabled " : ""
+    } inline-flex items-center gap-x-1.5 rounded-md bg-devhub-green px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
     onClick={props.onClick}
+    disabled={props.disabled}
   >
     <i
       className="-ml-0.5 h-5 w-5 bi bi-plus-circle-fill"
