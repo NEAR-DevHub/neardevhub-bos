2a5f244217712c21d8d5b7d5328d43b963904d37
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-05-23 14:32:00 +0530

    scroll author container in feed page

--- src/devhub/feature/proposal-search/by-author.jsx
+++ src/devhub/feature/proposal-search/by-author.jsx
@@ -6,7 +6,7 @@ if (!authorsOptions.length) {
   const authors = Near.view(
     "${REPL_DEVHUB_CONTRACT}",
     "get_all_proposal_authors",
-    {}
+    {},
   );
 
   if (Array.isArray(authors)) {
@@ -17,8 +17,15 @@ if (!authorsOptions.length) {
   }
 }
 
+const Container = styled.div`
+  .dropdown-menu {
+    max-height: 400px;
+    overflow-x: auto;
+  }
+`;
+
 return (
-  <div>
+  <Container>
     <Widget
       src="${REPL_DEVHUB}/widget/devhub.components.molecule.DropDown"
       props={{
@@ -31,5 +38,5 @@ return (
         selectedValue: props.author,
       }}
     />
-  </div>
+  </Container>
 );
