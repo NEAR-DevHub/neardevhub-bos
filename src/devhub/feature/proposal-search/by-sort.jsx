6d36d142fd8e320d7865645535fcfa3a7c1364d6
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-05-23 23:23:43 +0530

    Rename none option to all for feed filters (#787)
    * rename none option to all
    
    * fmt

--- src/devhub/feature/proposal-search/by-sort.jsx
+++ src/devhub/feature/proposal-search/by-sort.jsx
@@ -1,10 +1,11 @@
 const options = [
+  { label: "All", value: "" },
   { label: "Most recent", value: "proposal_id" }, // proposal_id desc
   { label: "Most viewed", value: "views" }, // views desc
   // TODO add track_comments function to devhub to track it in the indexer
   // { label: "Most commented", value: "" }, // comments desc
   // { label: "Unanswered", value: "" }, // where comments = 0
-  { label: "None", value: "" }, // where comments = 0
+  // where comments = 0
 ];
 
 const setSelected = props.onStateChange ?? (() => {});
