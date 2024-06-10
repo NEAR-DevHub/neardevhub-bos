de2e3c822eff2132cff9d802c9163fcf7d7fa772
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-06-10 18:18:20 +0000

    bos-workspace 1.0.0-alpha.27
    remove build folder from git

--- instances/devhub.near/build/src/widget/devhub.entity.addon.blogv2.Blog.jsx
+++
@@ -1,9 +0,0 @@
-const { blogId, template, handle } = props;
-const blog =
-  Social.get(`${handle}.community.devhub.near/blog/${blogId}/**`, "final") ||
-  {};
-if (!blog || Object.keys(blog).length === 0) {
-  return <div>Loading ...</div>;
-}
-const Template = template || (() => <></>);
-return <Template data={{ ...blog.metadata, content: blog[""], handle }} />;
--- instances/devhub.near/build/src/widget/devhub.page.proposals.jsx
+++
@@ -1,3 +0,0 @@
-return (
-  <Widget src={"devhub.near/widget/devhub.entity.proposal.Feed"} props={{}} />
-);
