72a2042992df6050a29e70b97069e34733d11dd8
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-06-11 18:51:19 +0000

    apply posthog api key from env

--- scripts/dev-gateway.mjs
+++ scripts/dev-gateway.mjs
@@ -1,9 +1,10 @@
 import httpServer from 'http-server';
 import path from 'path';
 import { spawn } from 'child_process';
-import { homedir } from 'os';
+import { homedir, tmpdir } from 'os';
+import { readFile, writeFile } from 'fs/promises';
 
-const instanceName = process.argv[process.argv.length-1];
+const instanceName = process.argv[process.argv.length - 1];
 const instanceFolder = `instances/${instanceName}`;
 
 // Start the HTTP server
@@ -15,9 +16,15 @@ server.listen(8080, () => {
   console.log('HTTP server is listening on port 8080');
 });
 
+const replacements = JSON.parse((await readFile(`${instanceFolder}/aliases.mainnet.json`)).toString());
+const replacementsWithEnvFileName = `${tmpdir()}/replacements.json`;
+await writeFile(replacementsWithEnvFileName, JSON.stringify(
+  Object.assign(replacements, { 'REPL_POSTHOG_API_KEY': process.env['POSTHOG_API_KEY'] })
+  , null, 1)
+);
 // Start the 'npm run dev' process
-const npmRunDev = spawn(`${homedir()}/.cargo/bin/bos-loader`, ['-p', `${instanceFolder}`, '-r', `${instanceFolder}/aliases.mainnet.json`, instanceName], { stdio: 'inherit' });
+const bosLoader = spawn(`${homedir()}/.cargo/bin/bos-loader`, ['-p', `${instanceFolder}/widget`, '-r', replacementsWithEnvFileName, instanceName], { stdio: 'inherit' });
 
-npmRunDev.on('close', (code) => {
-  console.log(`npm run dev process exited with code ${code}`);
+bosLoader.on('close', (code) => {
+  console.log(`bos-loader process exited with code ${code}`);
 });
