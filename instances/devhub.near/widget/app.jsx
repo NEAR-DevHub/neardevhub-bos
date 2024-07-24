22a9ba895c22632c68c41619047035aa000f5c1c
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-07-24 09:48:27 +0000

    testing a little change

--- instances/devhub.near/widget/app.jsx
+++ instances/devhub.near/widget/app.jsx
@@ -1,6 +1,7 @@
 /**
  * This is the main entry point for the DevHub application.
  * Page route gets passed in through params, along with all other page props.
+ *
  */
 
 const { onDraftStateChange } = VM.require(
