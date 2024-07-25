e848f31d86132e237d8f91c3690479a26bf8391b
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-07-25 19:45:55 +0000

    playwright test for share link for community announcement

---
+++ playwright-tests/tests/community/announcements.links.spec.js
@@ -0,0 +1,48 @@
+import { test, expect } from "@playwright/test";
+import { pauseIfVideoRecording } from "../../testUtils.js";
+import { mockDefaultTabs } from "../../util/addons.js";
+
+test.beforeEach(async ({ page }) => {
+  await page.route("http://localhost:20000/", async (route) => {
+    await mockDefaultTabs(route);
+  });
+});
+
+test.afterEach(
+  async ({ page }) => await page.unrouteAll({ behavior: "ignoreErrors" })
+);
+
+test.describe("Clipboard permissions", () => {
+  test.use({
+    contextOptions: {
+      permissions: ["clipboard-read", "clipboard-write"],
+    },
+  });
+
+  test("should share clean URL for announcements", async ({ page }) => {
+    await page.goto(
+      "/devhub.near/widget/app?page=community&handle=webassemblymusic&tab=announcements"
+    );
+    const copyUrlButton = await page
+      .locator('[data-component="near/widget/CopyUrlButton"]')
+      .first();
+    await expect(copyUrlButton).toBeVisible({ timeout: 10000 });
+    await copyUrlButton.hover();
+    await expect(await page.getByText("Copy URL to clipboard")).toBeVisible();
+    await copyUrlButton.click();
+
+    const linkUrlFromClipboard = await page.evaluate(
+      "navigator.clipboard.readText()"
+    );
+
+    await pauseIfVideoRecording(page);
+    await page.goto(linkUrlFromClipboard);
+
+    await expect(await page.getByText("WebAssembly Music")).toBeVisible({
+      timeout: 10000,
+    });
+    expect(linkUrlFromClipboard).toEqual(
+      "https://devhub.near.page/community/webassemblymusic/announcements?accountId=webassemblymusic.community.devhub.near&blockHeight=116026696"
+    );
+  });
+});
