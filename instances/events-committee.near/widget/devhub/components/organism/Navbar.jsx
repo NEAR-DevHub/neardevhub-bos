3ca5000d044323d768cbc7ca9f411fb3cc0bd79a
Thomas <thomasguntenaar@gmail.com>
2024-10-30 19:53:07 -0500

    Quick and dirty sorting for infra (#976)
    * fix: infra approved proposals to the top, other portals stay the same.
    
    * fix: hasModerator is not a function flash on events-committee.near

--- instances/events-committee.near/widget/devhub/components/organism/Navbar.jsx
+++ instances/events-committee.near/widget/devhub/components/organism/Navbar.jsx
@@ -4,9 +4,9 @@ const [showMenu, setShowMenu] = useState(false);
 
 const { href: linkHref } = VM.require("${REPL_DEVHUB}/widget/core.lib.url");
 
-const { hasModerator } =
-  VM.require("${REPL_EVENTS}/widget/core.adapter.devhub-contract") ||
-  (() => {});
+const { hasModerator } = VM.require(
+  "${REPL_EVENTS}/widget/core.adapter.devhub-contract"
+) || { hasModerator: () => {} };
 
 linkHref || (linkHref = () => {});
 
